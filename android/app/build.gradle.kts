21f914995c41ddb9a82c5d2a96fb421e887f1b3c
heamasoft <heama.soft@gmail.com>
2025-08-07 16:13:41 +0300

    Save local changes before pulling

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.harikar_app_ios"
+    namespace = "com.example.harikarappios"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.harikar_app_ios"
+        applicationId = "com.example.harikarappios"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
